bb9a9fca8ec99e07e42cf23e394b0410117cc664
TangLitEn <65808174+TangLitEn@users.noreply.github.com>
2023-11-19 17:02:10 +0800

    updated the robot file with the carForward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from maze import printMaze
 from maze import initMaze
 from robot import carDetection
+from robot import carForward
 
 route = [[0,0],[0,1],[0,2],[0,3],[0,4],
 	 [1,1],[2,1],[3,1],[4,1]]
@@ -9,4 +10,8 @@ maze_size_square = 5
 maze_array = initMaze(route,maze_size_square)
 printMaze(maze_array,maze_size_square)
 
-carDetection([2,2],route)
+print(carDetection([1,1],[0,1],maze_array))
+print(carForward([2,2],[0,1]))
+print(carForward([2,2],[0,-1]))
+print(carForward([2,2],[1,0]))
+print(carForward([2,2],[-1,0]))
--- robot.py
+++ robot.py
@@ -1,16 +1,23 @@
-def carDetection(car_location,route_array):
+def carDetection(car_location,direction_array,maze_array):
 	try:
+		X = direction_array[0]
+		Y = direction_array[1]
+
 		result = [
-			route_array[car_location[0]-1][car_location[1]-1],
-			route_array[car_location[0]-1][car_location[1]],
-			route_array[car_location[0]-1][car_location[1]+1],
-			route_array[car_location[0]][car_location[1]-1],
-			route_array[car_location[0]][car_location[1]],
-			route_array[car_location[0]][car_location[1]+1]
+			maze_array[car_location[0]-Y-X][car_location[1]-Y+X],
+			maze_array[car_location[0]-Y][car_location[1]+X],
+			maze_array[car_location[0]-Y+X][car_location[1]+Y+X],
+			maze_array[car_location[0]-X][car_location[1]-Y],
+			maze_array[car_location[0]][car_location[1]],
+			maze_array[car_location[0]+X][car_location[1]+Y]
 			]
-		print(result)
 		return result
 	except:
-		print("CRASH")
-		return ["X"]
+		return ["CRASH"]
 	
+def carForward(car_location,direction_array):
+	X = direction_array[0]
+	Y = direction_array[1]	
+	return [car_location[0]-Y,car_location[1]+X]
+	
+ 
